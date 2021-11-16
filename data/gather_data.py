d96e230ba132b8d7ec69e83029472a2f92859791
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-11-17 01:42:56 +0300

    gather datas

--- data/gather_data.py
+++ data/gather_data.py
@@ -1,8 +1,15 @@
 from icrawler.builtin import BingImageCrawler
 
-classes = ['girilmez trafik levhası']
+classes = ['girilmez trafik levhası','Hız sınırı','ileri mecburi yön','ileri sağa mecburi yön']
 number = 15
 
 for c in classes:
     bing_crawler = BingImageCrawler(storage={'root_dir':f'p{c.replace(" ","")}'})
+    bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)
+
+
+classes=['roads','cars']
+number=20
+for c in classes:
+    bing_crawler=BingImageCrawler(storage={'root_dir':f'n/{c.replace(" ","")}'})
     bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)
