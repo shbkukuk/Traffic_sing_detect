ca067506bc498e6f58b27264b91f85a552d5ee2b
SÜHA BERK KUKUK <shbkukuk@gmail.com>
2021-11-17 16:01:34 +0300

    All type is completed

--- data/gather_data.py
+++ data/gather_data.py
@@ -1,15 +1,15 @@
 from icrawler.builtin import BingImageCrawler
 
-classes = ['girilmez trafik levhası','Hız sınırı','ileri mecburi yön','ileri sağa mecburi yön']
-number = 15
+classes = ['turn right ahead sign']
+number = 20
 
 for c in classes:
     bing_crawler = BingImageCrawler(storage={'root_dir':f'p{c.replace(" ","")}'})
     bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)
 
 
-classes=['roads','cars']
+"""classes=['roads','cars']
 number=20
 for c in classes:
     bing_crawler=BingImageCrawler(storage={'root_dir':f'n/{c.replace(" ","")}'})
-    bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)
+    bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)"""
--- main.py
+++ main.py
@@ -3,20 +3,45 @@ from matplotlib import  pyplot as plt
 
 img1 = cv.imread("images/4.jpg")
 girilmez_data = cv.CascadeClassifier('data/girilmezlevha/classifier/cascade.xml')
+hizsiniri_data = cv.CascadeClassifier('data/hizsiniri/classifier/cascade.xml')
+ilerisag_data = cv.CascadeClassifier('data/sagamecburiyon/classifier/cascade.xml')
+ilerimecburi_data = cv.CascadeClassifier('data/ilerimecburiyon/classifier/cascade.xml')
 img1_gray = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 img1_rgb = cv.cvtColor(img1, cv.COLOR_BGR2RGB)
 
+girilmez =girilmez_data.detectMultiScale(img1_gray,minSize=(20,20))
+ilerisag= ilerisag_data.detectMultiScale(img1_gray,minSize =(20, 20))
+hizsiniri = hizsiniri_data.detectMultiScale(img1_gray,minSize=(20,20))
+ilerimecburi = ilerimecburi_data.detectMultiScale(img1_gray,minSize=(20,20))
 
-found = girilmez_data.detectMultiScale(img1_gray,minSize =(20, 20))
-amount_found = len(found)
-if amount_found != 0:
-    for (x, y, width, height) in found:
+
+if len(ilerisag) != 0:
+    for (x, y, width, height) in ilerisag:
         font = cv.FONT_HERSHEY_SIMPLEX
-        cv.putText(img1_rgb,'Girilmez Trafik Levhasi',(x,y+height),
+        cv.putText(img1_rgb,'Mecburi Saga Yon',(300,690),
                    font,0.75,(0,255,255),2,cv.LINE_AA)
-        #cv.rectangle(img1_rgb, (x, y),
-                      #(x + height, y + width),
-                      #(0, 255, 0), 5)
+elif len(girilmez) !=0:
+    for (x, y, width, height) in girilmez:
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(img1_rgb, 'Girilmez', (x, y + height),
+                   font, 0.75, (0, 255, 0), 2, cv.LINE_AA)
+
+elif len(hizsiniri) != 0:
+    for (x, y, width, height) in hizsiniri:
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(img1_rgb, 'Hiz Siniri', (x, y + height),
+                       font, 0.75, (0, 255, 0), 2, cv.LINE_AA)
+elif len(ilerimecburi) != 0:
+    for (x, y, width, height) in hizsiniri:
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(img1_rgb, 'Mecburi ileri Yon', (x, y + height),
+                       font, 0.75, (0, 255, 0), 2, cv.LINE_AA)
+
+else:
+    font = cv.FONT_HERSHEY_SIMPLEX
+    cv.putText(img1_rgb, 'Yol Yok', (195,260),
+               font, 0.75, (0, 255, 0), 2, cv.LINE_AA)
+
 
 
 plt.subplot(1, 1, 1)
